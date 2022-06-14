64973d73ea9b5e853f537efddb0274a00d2ee2a3
jonlee012 <jonlee86.jl@gmail.com>
2022-06-14 10:19:42 -0500

    jsp

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -7,13 +7,33 @@ import java.io.*;
 
 
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
 public class HelloWorldServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!!!!</h1>"+"<h2>How are you?</h2>");
-
+//    public String sayHello (String name){
+//        return "Hello " + name + "!";
+//    }
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+//        response.setContentType("text/html");
+//        PrintWriter out = response.getWriter();
+//        out.println("<h1>Hello, + name </h1>"+"<h2>How are you?</h2>");
+//
+//    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String name = request.getParameter("name");
+        // if the name is null, it means that "name" was not present in the query
+        // string, and here we'll provide a default value
+        if (name == null) {
+            name = "World!";
+        } else if (name.equals("bgates")) {
+            response.sendRedirect("https://microsoft.com");
+            return;
+        }
+        // pass the value of the name variable to the view, and send the request
+        // forward to the hello.jsp file
+        request.setAttribute("name", name);
+        request.getRequestDispatcher("/hello.jsp").forward(request, response);
     }
+
+
 }
